6e1bc6922d95401f8ad310fc48f7e64ddb0c9554
Katrina Holcomb <katrinaholcomb@Katrinas-MBP.domain>
2021-07-31 15:50:57 -0600

    adding schema and query

--- schema.sql
+++ schema.sql
@@ -54,3 +54,13 @@ CREATE TABLE titles (
 );
 
 SELECT * FROM departments;
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, from_date)
+);
